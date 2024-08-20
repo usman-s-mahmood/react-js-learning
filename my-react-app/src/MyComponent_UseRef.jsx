8812cdc09eb4fc2386fe1985d9f6f2943acc62de
usman.shahid <usmanshahid027@outlook.com>
2024-08-19 17:16:43 -0700

    completed useRef

--- my-react-app/src/MyComponent_UseRef.jsx
+++ my-react-app/src/MyComponent_UseRef.jsx
@@ -1,8 +1,30 @@
+import { useEffect, useRef, useState } from "react";
+
 let MyComponent_UseRef = () => {
+    // let [number, setNumber] = useState(0);
+    // const ref = useRef(0);
+    const inputRef = useRef(null);
+
+    let handleClick = () => {
+        inputRef.current.focus();
+        inputRef.current.style.backgroundColor = 'red';
+        console.log(inputRef.current);
+    }
+
+    useEffect(
+        () => {
+            console.log('component rendered!');
+            // console.log(inputRef);
+        }
+    );
     return (
         <>
         <div>
-
+            <p>Value of number: {}</p>
+            <button onClick={handleClick}>Click Me!</button>
+            <input type="text" 
+                ref={inputRef}
+            />
         </div>
         </>
     );
